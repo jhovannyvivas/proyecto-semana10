65a57c834ab01d347e19bacf856a03c393e059fe
jhovannyvivas <EDWIN.VIVAS@ALTERU.CO>
2021-07-27 16:49:47 -0500

    ensayo con onMouseEnter

--- src/Components/logo/Logo.js
+++ src/Components/logo/Logo.js
@@ -1,14 +1,15 @@
 import './Logo.css';
 import logoPokemon from '../../Assets/img/titulo3.png';
+import Funcion1 from './funcion1/Funcion1';
 
 export default function Logo(){
-    
-    
+    let cast = 'castidad'
     return(
-        <span className="headerImg">
-            <img src={logoPokemon}  alt="Logo"/>
+        <span className="headerImg" onMouseEnter={()=> alert('')}>
 
+            <Funcion1 pureza={cast?}/>
         </span>
     )
 }
 
+//<img src={logoPokemon}  alt="Logo"/>
---
+++ src/Components/logo/funcion1/Funcion1.js
@@ -0,0 +1,9 @@
+import './Funcion1.css'
+
+export default function Funcion1(props){
+
+    
+    return(
+        <h1 className='letras'>sss  {props.pureza}</h1>
+    )
+}
