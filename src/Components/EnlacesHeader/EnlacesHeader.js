e625fa6d359a2f3d70675d175c9d76735bf5987c
jhovannyvivas <EDWIN.VIVAS@ALTERU.CO>
2021-07-28 17:50:07 -0500

    Homepage terminado

--- src/Components/EnlacesHeader/EnlacesHeader.js
+++ src/Components/EnlacesHeader/EnlacesHeader.js
@@ -6,7 +6,7 @@ export default function EnlacesHeader () {
     return(
         <div className='EnlacesHeader'>
         <Link  className='Link'  to='/aboutus'><span className= "headerA"><p className='letras'>Quiénes somos</p></span>  </Link>
-        <Link className='Link' to='/contact'><span className= "headerA" id='contact'><p className='letras'>Quiénes somos</p></span></Link>          
+        <Link className='Link' to='/contact'><span className= "headerA" id='contact'><p className='letras'>Contáctanos</p></span></Link>          
                     
         </div>
     )
