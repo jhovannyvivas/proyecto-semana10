82165e3528d35d8672eac8064555cb5b029c76b8
klodskov <santiago.jimenez@aleru.co>
2021-07-28 20:12:58 -0500

    se designo espacio a la pagina de resultados

--- src/views/Card-detail/index.jsx
+++ src/views/Card-detail/index.jsx
@@ -2,10 +2,23 @@ import './Card-detail.scss';
 import React from 'react';
 
 export default function Detalles() {
+
     return(<div className= "container-detalles">
-        <h2>Estas son las estadisticas de tu pokemon</h2>
-            <div>
-                <img src="" alt="Tu carta pokémon" />
+            
+            
+            <h2>Estas son las estadisticas de tu pokémon!</h2>
+            
+            <div className="name-container">
+                <p>*el nombre de tu Pokémon*</p>
+            </div>
+            
+            <div className="card-container">
+             
+            </div>
+
+            <div className="stats-container">
+                <div><p>*tipo del pokémon*</p></div>
+                <div><p>*vida del pokémon*</p></div>
             </div>
 
 
